74ac349b590fc19f540ef1c09afd8a77293ad732
austen freeze <austentaylorfreeze@gmail.com>
2025-03-09 05:10:31 -0400

    xsten.co

--- my-next-app/src/app/layout.tsx
+++ my-next-app/src/app/layout.tsx
@@ -1,4 +1,6 @@
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 export const metadata = {
   title: "My Next App",
@@ -13,6 +15,8 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body>{children}</body>
+      <SpeedInsights/>
+      <Analytics/>
     </html>
   );
 }
